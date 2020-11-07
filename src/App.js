59d2caea5df169aaf81a6ce44877385a9e0d5a50
artyomsveshnikov <zenitfan219@gmail.com>
2020-11-07 13:17:11 +0300

    Edited content of site

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This example site was created by Artyom Sveshnikov.
+          This example site was created by Artyom Sveshnikov!
         </p>
         <a
           className="App-link"
